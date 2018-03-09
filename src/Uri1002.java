7c670fc5444db235ee73cc9f7a2efe6cafaece8d
aluno <aluno@LAB421.udicentro.local>
2018-03-08 22:25:20 -0300

    Uri

--- src/Uri1002.java
+++ src/Uri1002.java
@@ -1,4 +1,19 @@
-
+import java.io.IOException;
+import java.util.Scanner; 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
 public class Uri1002 {
-
+ 
+    public static void main(String[] args) throws IOException {
+ 		Scanner sc = new Scanner(System.in);
+		double I = 3.14159;
+		double R = sc.nextDouble();
+		double A = R * R * I;
+		System.out.printf("A=%.4f\n",A);
+		sc.close();
+	}
 }
