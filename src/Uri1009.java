8f9edc5d7cd00737edb5ec8a25172855aa818fe7
aluno <aluno@LAB421.udicentro.local>
2018-03-08 22:19:55 -0300

    alterações Uri 1009 a 1045

--- src/Uri1009.java
+++ src/Uri1009.java
@@ -1,4 +1,13 @@
+import java.util.Scanner;
 
 public class Uri1009 {
-
+   public static void main(String args[]) {
+   Scanner sc = new Scanner(System.in);
+   String A = sc.nextLine();
+   double B = sc.nextDouble();
+   double C = sc.nextDouble();
+   double X = (C * 0.15) + B;
+   System.out.printf("TOTAL = R$ %.2f\n",X);
+   sc.close();
+   }    
 }
